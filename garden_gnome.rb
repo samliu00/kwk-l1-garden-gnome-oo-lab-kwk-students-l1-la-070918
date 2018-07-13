e6488ec392de716f70f0138543b9c8cbc3ae842f
samliu00 <samanthaliu19@gmail.com>
2018-07-13 16:25:54 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,34 +1,11 @@
 # Code your instances here
 
 class GardenGnome
-  def name=(gnome_name)
-    @name = gnome_name
-  end 
-  def name
-    @name 
-  end 
-  def age=(gnome_age)
-    @age = gnome_age
-  end
-  def age
-    @age
-  end 
-  def gluten_allergy=(allergy)
-    @gluten_allergy = allergy
-  end 
-  def gluten_allergy
-    @gluten_allergy
-  end 
+
   def initialize(personality = "evil", hat_color = "red")
     @personality = "evil"
     @hat_color = hat_color
   end 
-  def hat_color = (hat_color)
-    @hat_color = hat_color
-  end
-  def hat_color
-    @hat_color
-  end
   def gnaw
     return "Gnawing on a tree!!!"
   end 
