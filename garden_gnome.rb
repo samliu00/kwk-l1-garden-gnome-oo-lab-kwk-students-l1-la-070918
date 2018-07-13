7ac7fa972eea9fa31ae5e5a872943d9b0646f312
samliu00 <samanthaliu19@gmail.com>
2018-07-13 16:19:03 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,33 @@
 # Code your instances here
+
+class GardenGnome
+  def name=(gnome_name)
+    @name = gnome_name
+  end 
+  def name
+    @name 
+  end 
+  def age=(gnome_age)
+    @age = gnome_age
+  end
+  def age
+    @age
+  end 
+  def gluten_allergy=(allergy)
+    @gluten_allergy = allergy
+  end 
+  def gluten_allergy
+    @gluten_allergy
+  end 
+  def initialize(personality, hat_color)
+    @personality = "evil"
+    @hat_color = "red"
+  end 
+  def hat_color = (hat_color)
+    @hat_color = hat_color
+  end
+  def hat_color
+    @hat_color
+  end
+  def 
+  
